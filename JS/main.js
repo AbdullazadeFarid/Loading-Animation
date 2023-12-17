01364d97898b3aeed676231f7b7a972e5a4e1041
AbdullazadeFarid <abdullazadefarid@mail.ru>
2023-12-17 16:32:04 +0300

    first commit

---
+++ JS/main.js
@@ -0,0 +1,39 @@
+
+
+// var overlay = document.querySelector(".overlay")
+
+
+
+// var small_around = document.querySelector(".small_around");
+// var btn = document.querySelector(".btn");
+
+// btn.addEventListener("click", function(){
+//     small_around.classList.add("around");
+//     overlay.classList.add("active"); // klik eledikde overlay i gosteririk
+
+//     setTimeout(function() {
+//         small_around.classList.add("hidden");
+//         overlay.classList.remove("active"); // klik eledikde obverlay i silirik
+//     }, 5000);
+// });
+
+
+
+
+
+var overlay = document.querySelector(".overlay")
+
+
+
+var small_around = document.querySelector(".small_around");
+var btn = document.querySelector(".btn");
+
+
+    small_around.classList.add("around");
+    overlay.classList.add("active"); // klik eledikde overlay i gosteririk
+
+    setTimeout(function() {
+        small_around.classList.add("hidden");
+        overlay.classList.remove("active"); // klik eledikde obverlay i silirik
+    }, 5000);
+
